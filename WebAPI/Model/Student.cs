09e252af88bff557c2c5de54d6decf2c456504f8
Andrés Rojas <anrj3099@hotmail.com>
2019-06-09 13:22:30 -0400

    Configuración de EntityFramework

---
+++ WebAPI/Model/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Model
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+    }
+}
---
+++ WebAPI/Persistence/StudentDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Persistence
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {}
+    }
+}
