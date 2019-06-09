b42739ba4dc1e88a30d6d177da5b8ca2a6ac7fce
Andrés Rojas <anrj3099@hotmail.com>
2019-06-09 14:22:51 -0400

    Cambio de ruteo.

--- WebAPI/WebAPI/Startup.cs
+++ WebAPI/WebAPI/Startup.cs
@@ -47,7 +47,12 @@ namespace WebAPI
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
